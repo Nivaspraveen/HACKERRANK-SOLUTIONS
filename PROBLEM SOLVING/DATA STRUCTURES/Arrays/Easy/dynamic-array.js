781d0a9e5e955e6c7a26eb9a96b56d0b27edf7ad
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-23 11:53:09 +0530

    Added solution for Data Structures - Arrays

---
+++ PROBLEM SOLVING/DATA STRUCTURES/Arrays/Easy/dynamic-array.js
@@ -0,0 +1,71 @@
+// Solution for Dynamic Array
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function dynamicArray(n, queries) {
+    const seqList = Array.from({ length: n }, () => []), results = [];
+    let lastAns = 0;
+    for (const query of queries) {
+        const [type, x, y] = query, idx = (x ^ lastAns) % n;
+        if (type === 1) seqList[idx].push(y);
+        else if (type === 2) {
+            const value = seqList[idx][y % seqList[idx].length];
+            lastAns = value;
+            results.push(lastAns);
+        }
+    }
+    return results
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const q = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(q);
+
+    for (let i = 0; i < q; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = dynamicArray(n, queries);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
---
+++ PROBLEM SOLVING/DATA STRUCTURES/Arrays/Hard/array-manipulation.js
@@ -0,0 +1,70 @@
+// Solution for Array Manipulation
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function arrayManipulation(n, queries) {
+    const arr = Array(n + 1).fill(0);
+    for (const [a, b, k] of queries) {
+        arr[a] += k;
+        if (b + 1 <= n) arr[b + 1] -= k;
+    }
+    let maxSum = 0, currSum = 0;
+    for (let i = 1; i <= n; i++) {
+        currSum += arr[i];
+        maxSum = Math.max(maxSum, currSum);
+    } 
+    return maxSum;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = arrayManipulation(n, queries);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
