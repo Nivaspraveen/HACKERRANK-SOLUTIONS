ffe9f436bbbf6ca949ddfdf31c6803900bd14955
Nivaspraveen <nivaspraveen07@gmail.com>
2024-11-23 12:02:52 +0530

    Added solutions for Implemention Basic already completed

---
+++ PROBLEM SOLVING/ALGORITHMS/Implemntation/Easy/between-two-sets.js
@@ -0,0 +1,79 @@
+// Solution for Between Two Sets
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function gcd(x, y) {
+    while (y !== 0) {
+        let temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
+
+function lcm(x, y) {
+    return (x * y) / gcd(x, y);
+}
+
+function getTotalX(a, b) {
+    let lcmVal = a[0];
+    for (let i = 1; i < a.length; i++) lcmVal = lcm(lcmVal, a[i]);
+    let gcdVal = b[0];
+    for (let i = 1; i < b.length; i++) gcdVal = gcd(gcdVal, b[i]);
+    let count = 0;
+    for (let i = lcmVal; i <= gcdVal; i +=lcmVal) {
+        if (gcdVal % i === 0) count++;
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
